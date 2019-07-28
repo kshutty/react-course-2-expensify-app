75a4187c949408ee862c3b763cb4f5da5f142321
kyle shuttleworth <shuttleworth0428@yahoo.com>
2019-07-28 03:20:29 -0400

    up to tutorial 134 7 min plus external modifications to webpack were made

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,51 @@
 // entry -> output 
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = (env) =>{
+    const isProduction = env === 'production'
+    const CSSExtract = new MiniCssExtractPlugin( 'styles.css')
 
-module.exports ={
-entry: './src/app.js',
-output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-},
-module:{
-rules: [{
-    loader: 'babel-loader',
-    test: /\.js$/,
-    exclude: /node_modules/
-}, {
-    test: /\.s?css$/,
-    use: [
-    'style-loader', 
-    'css-loader',
-    'sass-loader'
-    ]
-}]
-},
-devtool: 'cheap-module-eval-source-map',
-
-devServer: {
-    contentBase: path.join(__dirname,'public'),
-    historyApiFallback: true
+   return  {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module:{
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
+            ]
+        }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            historyApiFallback: true
+        }
+    }
 }
-};
+
